bd8f211149d09f3347df6b8ef5d4aef0f5181993
Affan Sayeed <affansayeed234@gmail.com>
2025-07-26 11:14:39 +0530

    Fixing Bug

--- frontend/src/utils/getDriverData.js
+++ frontend/src/utils/getDriverData.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 
-const driverToken = localStorage.getItem("driverToken");
-const decoded = driverToken && jwtDecode(driverToken);
 
 export default async function getDriverInfo() {
+  const driverToken = localStorage.getItem("driverToken");
+  const decoded = driverToken && jwtDecode(driverToken);
   const res = await axios.get(`https://rideonix-backend.onrender.com/api/driver/${decoded.id}`);
   return res.data.data;
 }
